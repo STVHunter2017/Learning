eb324a22632a2c1ff759c0060388852acfc31b17
STV:W <steven.hyles@johncrane.com>
2017-02-10 07:55:44 +0000

    Removed example code

--- Angular_2/app/events/event-thumbnail.component.ts
+++ Angular_2/app/events/event-thumbnail.component.ts
@@ -13,8 +13,7 @@ import {Component, Input, Output, EventEmitter } from '@angular/core'
                         <span>Location: {{event.location.address}}</span>
                         <span>&nbsp;</span>
                         <span>{{event.location.city}}, {{event.location.country}}</span>
-                    </div>
-                    <button class="btn btn-primary" (click)="handleClickMe()">Click me!</button>
+                    </div>                    
         </div>    
             `
 })
@@ -22,8 +21,4 @@ import {Component, Input, Output, EventEmitter } from '@angular/core'
 export class EventThumnailComponent{
     @Input() event: any
     @Output() eventClick = new EventEmitter()
-
-    handleClickMe(){
-        this.eventClick.emit(this.event.name)
-    }
 }
