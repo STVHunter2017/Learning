17dd4b27db5b069f7da0a55dfc0973002272ef6c
STV:W <steven.hyles@johncrane.com>
2017-02-17 07:40:13 +0000

    Pulling parameters from the url

--- Angular_2/app/events/event-details/event-details.component.ts
+++ Angular_2/app/events/event-details/event-details.component.ts
@@ -1,6 +1,7 @@
 //events/1
 import {Component } from '@angular/core'
 import {EventService} from '../shared/event.service'
+import {ActivatedRoute} from '@angular/router'
 
 @Component ({
     templateUrl: './app/events/event-details/event-details.component.html',
@@ -12,9 +13,9 @@ import {EventService} from '../shared/event.service'
 
 export class EventDetailsComponent {
     event : any
-    constructor(private eventService:EventService){        
+    constructor(private eventService:EventService, private route:ActivatedRoute){        
     }
     ngOnInit(){
-        this.event = this.eventService.getEvent(1)
+        this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])  //Wow!!  + is a cast operator, parameters off the current route.
     }
 }
--- Angular_2/app/routes.ts
+++ Angular_2/app/routes.ts
@@ -4,6 +4,6 @@ import {EventDetailsComponent} from './events/event-details/event-details.compon
 
 export const appRoutes: Routes = [
     {path: 'events', component: EventsListComponent},
-    {path: 'events/:id', component: EventDetailsComponent},
+    {path: 'events/:id', component: EventDetailsComponent},  //The colon forces the parameter to be read from the URL and be called id
     {path: '', redirectTo:'/events', pathMatch: 'full'},  //Full -extact match or Prefix - starts with    
 ]
