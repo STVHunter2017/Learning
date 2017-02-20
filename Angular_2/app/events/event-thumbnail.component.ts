262dde360b55bfa0b60f968e2b7e539d37a1f033
STV:W <steven.hyles@johncrane.com>
2017-02-20 08:25:18 +0000

    Reverted to just have ngClass

--- Angular_2/app/events/event-thumbnail.component.ts
+++ Angular_2/app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import {Component, Input } from '@angular/core'
          <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
                 <h2>{{event?.name}}</h2>
                     <div>Date : {{event?.date}}</div>
-                    <div class="well" [ngClass]="getStartTimeClass()" [ngSwitch] = "event?.time"> 
+                    <div [ngClass]="getStartTimeClass()" [ngSwitch] = "event?.time"> 
                         Time : {{event?.time}}
                     
                         <span *ngSwitchCase="'8:00 am'" >(Early Start)</span>
