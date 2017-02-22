a8a439bfb60ecb2ec15a2d6344824ad490ac0a4a
STV:W <steven.hyles@johncrane.com>
2017-02-22 08:03:10 +0000

    Ensured code finally uses just ngStyle

--- Angular_2/app/events/event-thumbnail.component.ts
+++ Angular_2/app/events/event-thumbnail.component.ts
@@ -5,8 +5,8 @@ import {Component, Input } from '@angular/core'
     template: `
          <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
                 <h2>{{event?.name}}</h2>
-                    <div [ngStyle]= "getStartTimeStyle()">Date : {{event?.date}}</div>
-                    <div [ngClass]="getStartTimeClass()" [ngSwitch] = "event?.time"> 
+                    <div>Date : {{event?.date}}</div>
+                    <div [ngStyle]="getStartTimeStyle()" [ngSwitch] = "event?.time"> 
                         Time : {{event?.time}}
                     
                         <span *ngSwitchCase="'8:00 am'" >(Early Start)</span>
@@ -28,8 +28,6 @@ import {Component, Input } from '@angular/core'
         </div>    
             `,
   styles:[`
-    .green {color: green !important;}
-    .bold {font-weight: bold;}
     .pad-left {margin-left: 10px;}   
      .well div {color: #bbb}
      .thumbnail {min-height :210px}
