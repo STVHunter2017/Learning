d245fefb5be99c06299843e267171fcda2e7a35a
STV:W <steven.hyles@johncrane.com>
2017-02-28 07:45:29 +0000

    Added orange indicator to navbar to show active link

--- Angular_2/app/Nav/navbar.component.ts
+++ Angular_2/app/Nav/navbar.component.ts
@@ -7,9 +7,12 @@ import {Component } from '@angular/core'
         .nav.navbar-nav {font-size : 15px;}
         #searchForm {margin-right : 100px;}
         @media (max-width: 1200px) {#searchForm {display: none}}
+        li > a.active {color: #F97924;}
     `]
 })
 
 export class NavbarComponent{
 
 }
+
+//li is to prevent this being overritten by bootstrap
