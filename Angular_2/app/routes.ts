6430b9ee9c28afc42cee92ebfe7fbfdabdd8d8f1
STV:W <steven.hyles@johncrane.com>
2017-03-01 07:53:40 +0000

    Added second module that handles users, made it lazy load and supply routes

--- Angular_2/app/routes.ts
+++ Angular_2/app/routes.ts
@@ -12,9 +12,13 @@ export const appRoutes: Routes = [
     {path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},  //The colon forces the parameter to be read from the URL and be called id    
     {path: '404', component:Error404Component},
     {path: '', redirectTo:'/events', pathMatch: 'full'},  //Full -extact match or Prefix - starts with    
+
+    {path : 'user', loadChildren: 'app/user/user.module#UserModule'}
     
 ]
 
 //square brackets are needed around [EventRouteActivator], for some reason
 //resolve is passed an object called events that is set from the EventsListResolverService
 //resolve adds them to a property on the route.
+
+//When the path starts with user, load the UserModule from the supplied path
